2b1a5d52dedd69d6497cd694889e885e97e843b1
Lucyta Qutsyaning Rosydah <lucytaqr08@gmail.com>
2024-07-29 17:52:51 +0700

    register layout

--- app/src/main/java/com/example/fitfoood/view/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/fitfoood/view/signup/SignUpActivity.kt
@@ -37,17 +37,17 @@ class SignUpActivity : AppCompatActivity() {
         binding.LoginTextView.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
-        binding.dateEditText.setOnClickListener {
-            showDatePickerDialog()
-        }
-
-        binding.genderRadioGroup.setOnCheckedChangeListener { group, checkedId ->
-            gender = if (checkedId == R.id.radioMan) {
-                "Male"
-            } else {
-                "Female"
-            }
-        }
+//        binding.dateEditText.setOnClickListener {
+//            showDatePickerDialog()
+//        }
+//
+//        binding.genderRadioGroup.setOnCheckedChangeListener { group, checkedId ->
+//            gender = if (checkedId == R.id.radioMan) {
+//                "Male"
+//            } else {
+//                "Female"
+//            }
+//        }
 
         setupView()
         setupAction()
@@ -118,7 +118,7 @@ class SignUpActivity : AppCompatActivity() {
             calendar.set(Calendar.MONTH, month)
             calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
             dateBirth = SimpleDateFormat("dd-MM-yyyy", Locale.US).format(calendar.time)
-            binding.dateEditText.setText(dateBirth) // Update EditText with selected date
+//            binding.dateEditText.setText(dateBirth) // Update EditText with selected date
         }
 
         DatePickerDialog(this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show()
